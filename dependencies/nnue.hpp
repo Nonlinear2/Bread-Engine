d4cdb4a97cf9421753347fde62ab3c7dba5b0a35
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-07-01 13:41:56 +0200

    add incbin to load nnue, and refactor
    Results of bread_engine_1.4.7 vs bread_engine_1.4.6 (8+0.8, NULL, NULL, UHO_2024_8mvs_big_+115_+134.pgn):
    Elo: 0.97 +/- 6.96, nElo: 1.39 +/- 9.96
    LOS: 60.74 %, DrawRatio: 42.18 %, PairsRatio: 1.02
    Games: 4670, Wins: 1539, Losses: 1526, Draws: 1605, Points: 2341.5 (50.14 %)
    Ptnml(0-2): [154, 515, 985, 526, 155], WL/DD Ratio: 2.49
    LLR: 0.75 (25.5%) (-2.94, 2.94) [-5.00, 0.00]

--- dependencies/nnue.hpp
+++ dependencies/nnue.hpp
@@ -5,16 +5,15 @@
 #include <fstream>
 #include <string>
 #include <cmath>
+#include <cstdint>
+#include <stdio.h>
 #include <chess.hpp>
 #include <immintrin.h>
-#include "neural_network.hpp"
 
 constexpr int num_avx_registers = 16;
 constexpr int int32_per_reg = 8;
 constexpr int int16_per_reg = 16;
 constexpr int int8_per_reg = 32;
-
-enum LayerName {FEATURE_TRANSFORMER, LAYER_1, LAYER_2, LAYER_3};
 
 inline const int HKP_size = 40960; // 64*64*5*2
 inline const int acc_size = 256;
@@ -60,43 +59,6 @@ struct modified_features {
         captured(captured) {};
 };
 
-template<typename in_type, int in_size, typename out_type, int out_size>
-class NNLayer {
-    public:
-
-    static constexpr int input_size = in_size;
-    static constexpr int output_size = out_size;
-
-    NNLayer();
-    ~NNLayer();
-
-    in_type* weights; // be careful, this is a flattened 2d array, to be contiguous in memory.
-    // otherways it would be an array of pointers pointing to scattered memory locations, 
-    // which would be slower and unpractical.
-    // weights are stored in row major
-
-    out_type* bias;
-
-    void load_from_header(LayerName name);
-};
-
-class FeatureTransformer: public NNLayer<int16_t, HKP_size, int16_t, acc_size> {};
-
-template<int in_size, int out_size>
-class HiddenLayer: public NNLayer<int8_t, in_size, int32_t, out_size> {
-    public:
-
-    static constexpr int num_input_chunks = in_size/int8_per_reg;
-    static constexpr int num_output_chunks = out_size/int32_per_reg;
-
-    void run(int8_t* input, int32_t* output);
-};
-
-class OutputLayer: public NNLayer<int8_t, 32, int16_t, 1> {
-    public:
-    int16_t run(int8_t* input);
-};
-
 class Accumulator {
     public:
     int16_t* operator[](bool color);
@@ -108,48 +70,119 @@ class NNUE {
     public:
     Accumulator accumulator; // array stored on the stack, as it will change often
     
-    FeatureTransformer feature_transformer = FeatureTransformer(); // 2*HKP_size -> 2*acc_size 
+    /*****************
+    Feature transformer
+    ******************/
+
+    // 2*HKP_size -> 2*acc_size 
+
+    static constexpr int ft_input_size = HKP_size;
+    static constexpr int ft_output_size = acc_size;
+
+    // weights are flattened 2d array, to be contiguous in memory.
+    // otherways it would be an array of pointers pointing to scattered memory locations, 
+    // which would be slower and unpractical.
+    // weights are stored in row major
+    int16_t* ft_weights = static_cast<int16_t*>(
+        operator new[](sizeof(int16_t)*ft_input_size*ft_output_size, std::align_val_t{32})
+    );
+    int16_t* ft_bias = static_cast<int16_t*>(
+        operator new[](sizeof(int16_t)*ft_output_size, std::align_val_t{32})
+    );
+
     // all computations happen in int16. Scale is 127
     // to make sure that even after accumulation no overflows happen : there can be a maximum of 30 active input features,
     // so we need (sum of 30 weights) + bias < 32767
     // we need to make sure this limit is not reached before converting the model.
-    
+
     // unclipped output is in accumulator
 
     // apply crelu16 and store
     int8_t ft_clipped_output[acc_size*2];
 
+    /******
+    Layer 2
+    *******/
+
+    // 512 -> 32
+
     // int8 weights with scale 64. Multiplication outputs in int16, so no overflows,
     // and sum is computed in int32. Maximum weights times maximum input with accumulation is 127*127*512 = 8258048
     // maximum bias is therefore (2,147,483,647-8,258,048)/32 = 66850799 which is totally fine.
-    HiddenLayer<512, 32> layer_2 = HiddenLayer<512, 32>(); // 512 -> 32
+    
+    static constexpr int l2_input_size = 512;
+    static constexpr int l2_output_size = 32;
+
+    int8_t* l2_weights = static_cast<int8_t*>(
+        operator new[](sizeof(int8_t)*l2_input_size*l2_output_size, std::align_val_t{32})
+    );
+    int32_t* l2_bias = static_cast<int32_t*>(
+        operator new[](sizeof(int32_t)*l2_output_size, std::align_val_t{32})
+    );
+
     // also, output is scaled back by 64, so total scale is still only 127. as we only do integer division,
     // error caused by the division is max 1/127.
-
-    int32_t layer_2_unclipped_output[32];
-
+    
+    int32_t l2_unclipped_output[32];
+    
     // apply crelu32 and store
-    int8_t layer_2_clipped_output[32];
+    int8_t l2_clipped_output[32];
+
+    /******
+    Layer 3
+    *******/
+    // 32 -> 32
+
+    static constexpr int l3_input_size = 32;
+    static constexpr int l3_output_size = 32;
+
+    int8_t* l3_weights = static_cast<int8_t*>(
+        operator new[](sizeof(int8_t)*l3_input_size*l3_output_size, std::align_val_t{32})
+    );
+    int32_t* l3_bias = static_cast<int32_t*>(
+        operator new[](sizeof(int32_t)*l3_output_size, std::align_val_t{32})
+    );
+
 
     // max value is 32*(127*127) with 32 accumulations of the max possible output with the max possible weight.
     // this is 516128. Max bias is therefore (2,147,483,647 - 516,128)/32 = 67092734 which is fine.
-    HiddenLayer<32, 32> layer_3 = HiddenLayer<32, 32>(); // 32 -> 32
+
     // still output is scaled back by 64, so scale is 127 times true output.
-    int32_t layer_3_unclipped_output[32];
-
+    int32_t l3_unclipped_output[32];
+   
     // apply crelu32 and store
-    int8_t layer_3_clipped_output[32];
-
+    int8_t l3_clipped_output[32];
+   
+    /******
+    Layer 4
+    *******/
+    // 32 -> 1
     
+    static constexpr int l4_input_size = 32;
+    static constexpr int l4_output_size = 1;
+
+    int8_t* l4_weights = static_cast<int8_t*>(
+        operator new[](sizeof(int8_t)*l4_input_size*l4_output_size, std::align_val_t{32})
+    );
+    int16_t* l4_bias = static_cast<int16_t*>(
+        operator new[](sizeof(int16_t)*l4_output_size, std::align_val_t{32})
+    );
+
+
     // max output value is 1*(127*127) + bias, the max possible output with the max possible weight.
     // this is 16129+bias which is less than the max int16 if bias is less than (int16_max - 16129)/(127*64) = 2.04
-    OutputLayer layer_4 = OutputLayer(); // 32 -> 1
+
+    // for layers 2 and 3:
+    void run(int8_t* input, int32_t* output, int input_size, int output_size, int8_t* weights, int32_t* bias);
+
     // output is not scaled back by 64, so scale is 64*127 times true output.
+    int16_t run_output_layer(int8_t* input, int8_t* weights, int16_t* bias);
 
     void crelu16(int16_t *input, int8_t *output, int size);
     void crelu32(int32_t *input, int8_t *output, int size);
 
     NNUE();
+    ~NNUE();
 
     void load_model();
 
