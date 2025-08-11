bf3edaa200da322aee67905f695e611f833230c2
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-08-11 23:08:57 +0200

    take 4

--- src/core.cpp
+++ src/core.cpp
@@ -436,12 +436,12 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss){
 
         if (is_valid(max_value) && !is_loss(max_value)){
             // lmp
-            if (!pv && !in_check && !is_capture && move_gen.index() > 2 + depth + improving 
+            if (!in_check && !is_capture && move_gen.index() > 2 + depth + improving 
                 && !is_hit && eval - lmp_1 * !improving < alpha)
                 continue;
 
             // SEE pruning
-            if (!pv && !in_check && !is_capture && !is_killer && move_gen.index() > 5 + depth / 2
+            if (!in_check && !is_capture && !is_killer && move_gen.index() > 5 + depth / 2
                 && depth < 5 && !SEE::evaluate(pos, move, alpha - static_eval - see_1 - see_2*depth))
                 continue;
         }
