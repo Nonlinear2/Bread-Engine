ce029a80cfeb3cfb9ba6db4dcac5d108d4b6a860
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-09-01 13:00:39 +0200

    take 5
    bench: 2418245

--- src/core.cpp
+++ src/core.cpp
@@ -387,7 +387,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss){
             if (!is_capture && !in_check
                 && prev_piece != int(Piece::NONE)
                 && prev_to != int(Square::underlying::NO_SQ)
-                && move_gen.cont_history.get(prev_piece, prev_to, pos.at(move.from()), move.to()) < -8000 - 500 * depth)
+                && move_gen.cont_history.get(prev_piece, prev_to, pos.at(move.from()), move.to()) < -7500 - 300 * depth)
                 continue;
         }
 
