9ba40a120438b7dd9e1200792043f60e747e08d7
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-10-13 22:09:09 +0200

    update
    bench: 2352814

--- src/piece_square_tables.hpp
+++ src/piece_square_tables.hpp
@@ -15,17 +15,7 @@ constexpr std::array<int, 64> pawn_map = {
            196,  196,  196,  196,  196,  196,  196,  196, 
            252,  252,  252,  252,  252,  252,  252,  252};
 
-constexpr std::array<int, 64> king_map_mg_b = { // middle game
-           -1,  -2,  -2,  -2,  -2,  -2,  -2,  -1, 
-           -1,  -2,  -2,  -2,  -2,  -2,  -2,  -1, 
-           -1,  -2,  -2,  -2,  -2,  -2,  -2,  -1, 
-           -1,  -2,  -2,  -2,  -2,  -2,  -2,  -1, 
-           -1,  -1,  -1,  -2,  -2,  -1,  -1,  -1, 
-           -0,  -1,  -1,  -1,  -1,  -1,  -1,  -0, 
-            2,   2,   0,   0,   0,   0,   2,   2, 
-            2,   3,   2,   0,   0,   2,   3,   2};
-
-constexpr std::array<int, 64> king_map_mg_w = { // middle game
+constexpr std::array<int, 64> king_map_mg = { // middle game
             2,   3,   2,   0,   0,   2,   3,   2, 
             2,   2,   0,   0,   0,   0,   2,   2, 
            -0,  -1,  -1,  -1,  -1,  -1,  -1,  -0, 
@@ -35,17 +25,7 @@ constexpr std::array<int, 64> king_map_mg_w = { // middle game
            -1,  -2,  -2,  -2,  -2,  -2,  -2,  -1, 
            -1,  -2,  -2,  -2,  -2,  -2,  -2,  -1};
 
-constexpr std::array<int, 64> king_map_eg_b = { // end game
-           -13,  -10,  -7,  -3,  -3,  -7,  -10,  -13, 
-           -7,  -3,   0,   3,   3,   0,  -3,  -7, 
-           -7,   0,   10,   13,   13,   10,   0,  -7, 
-           -7,   0,   13,   16,   16,   13,   0,  -7, 
-           -7,   0,   13,   16,   16,   13,   0,  -7, 
-           -7,   0,   10,   13,   13,   10,   0,  -7, 
-           -7,  -7,   3,   3,   3,   3,  -7,  -7, 
-           -13,  -7,  -7,  -7,  -7,  -7,  -7,  -13};
-
-constexpr std::array<int, 64> king_map_eg_w = { // end game
+constexpr std::array<int, 64> king_map_eg = { // end game
            -13,  -7,  -7,  -7,  -7,  -7,  -7,  -13, 
            -7,  -7,   3,   3,   3,   3,  -7,  -7, 
            -7,   0,   10,   13,   13,   10,   0,  -7, 
@@ -56,22 +36,17 @@ constexpr std::array<int, 64> king_map_eg_w = { // end game
            -13,  -10,  -7,  -3,  -3,  -7,  -10,  -13};
 
 struct PieceSquareMaps {
-    std::array<int, 4*64> all_ksm; // wking/bking, eg/not eg;
-
-
-    constexpr PieceSquareMaps(){
-        std::copy(king_map_mg_w.begin(), king_map_mg_w.end(), &all_ksm[0]);
-        std::copy(king_map_eg_w.begin(), king_map_eg_w.end(), &all_ksm[64]);
-        std::copy(king_map_mg_b.begin(), king_map_mg_b.end(), &all_ksm[128]);
-        std::copy(king_map_eg_b.begin(), king_map_eg_b.end(), &all_ksm[192]);
-    }
+    std::array<std::array<int, 64>, 2> all_ksm = {king_map_mg, king_map_eg};
 
     constexpr int get_pawn_psm(Color color, Square sq) const {
         return color == Color::WHITE ? pawn_map[sq.index()] : pawn_map[sq.flip().index()];
     }
 
     constexpr int get_ksm(Piece king, bool is_endgame, Square from_sq, Square to_sq) const {
-        int piece_idx = 128*(king == Piece::BLACKKING) + 64*is_endgame; 
-        return all_ksm[piece_idx + to_sq.index()] - all_ksm[piece_idx + from_sq.index()];
+        if (king.color() == Color::BLACK){
+            from_sq = from_sq.flip();
+            to_sq = to_sq.flip();
+        }
+        return all_ksm[is_endgame][to_sq.index()] - all_ksm[is_endgame][from_sq.index()];
     }
 };
