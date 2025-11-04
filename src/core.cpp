47e4b8034deb6dc6aa00b4dedbc694d41984affa
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-11-05 00:25:08 +0100

    take 1
    bench: 1735094

--- src/core.cpp
+++ src/core.cpp
@@ -441,10 +441,16 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
                 continue;
 
             // SEE pruning
-            if (!in_check && !is_killer && move_gen.index() > 5 + depth / 2
-                && depth < 5 && !SEE::evaluate(pos, move, alpha - static_eval - see_1 - see_2*depth))
-                continue;
-            
+            if (is_capture){
+                if (!in_check && !is_killer && move_gen.index() > 5 + depth / 2
+                    && depth < 7 && !SEE::evaluate(pos, move, alpha - static_eval - see_1 - see_2*depth))
+                    continue;
+            } else {
+                if (!in_check && !is_killer && move_gen.index() > 5 + depth / 2
+                    && depth < 5 && !SEE::evaluate(pos, move, alpha - static_eval - see_1 - see_2*depth))
+                    continue;
+            }
+
             // continuation history pruning
             if (!is_capture && !in_check
                 && prev_piece != int(Piece::NONE)
