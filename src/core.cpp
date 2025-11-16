3df4f433430dd9cee6179500d2c388f783990d8c
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-11-16 13:44:41 +0100

    update
    bench: 1950471

--- src/core.cpp
+++ src/core.cpp
@@ -219,7 +219,7 @@ Move Engine::iterative_deepening(SearchLimit limit){
 
         int asp_alpha;
         int asp_beta;
-        if (current_depth <= 4){
+        if (current_depth <= 6){
             asp_alpha = -INFINITE_VALUE;
             asp_beta = INFINITE_VALUE;
         } else {
@@ -232,11 +232,15 @@ Move Engine::iterative_deepening(SearchLimit limit){
             negamax<true>(current_depth, asp_alpha, asp_beta, root_ss, false);
             best_move = root_moves[0];
             
-            if (best_move.score() <= asp_alpha)
-                asp_alpha -= asp_beta - asp_alpha;
-            else if (best_move.score() >= asp_beta)
-                asp_beta += asp_beta - asp_alpha;
-            else
+            if (best_move.score() <= asp_alpha){
+                int difference = asp_beta - asp_alpha;
+                asp_beta -= difference / 2;
+                asp_alpha -= 3*difference / 2;
+            } else if (best_move.score() >= asp_beta){
+                int difference = asp_beta - asp_alpha;
+                asp_alpha += difference / 2;
+                asp_beta += 3 * difference / 2;
+            } else
                 break;
 
             asp_alpha = std::clamp(asp_alpha, -INFINITE_VALUE, INFINITE_VALUE);
