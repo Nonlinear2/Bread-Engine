f1c5d4a0580ef28e421515a5d9c143a489283902
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-11-18 00:06:43 +0100

    update
    bench: 1919507

--- src/core.cpp
+++ src/core.cpp
@@ -152,7 +152,7 @@ Move Engine::iterative_deepening(SearchLimit limit){
 
     int soft_time_limit = -1;
     if (limit.type == LimitType::Time)
-        soft_time_limit = 2 * limit.value / 3;
+        soft_time_limit = 4 * limit.value / 5;
 
     start_time = std::chrono::high_resolution_clock::now();
 
