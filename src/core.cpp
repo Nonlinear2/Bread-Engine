df633142b367659aa1d815fca555002313957362
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-11-25 21:38:57 +0100

    update
    bench: 1219672

--- src/core.cpp
+++ src/core.cpp
@@ -524,7 +524,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
         reduction += red_3 * (tt_capture && !is_capture);
         reduction += red_4 * (move_gen.index() > lmr_1);
         reduction += red_5 * (cutnode && depth > 7);
-        reduction += 600 * (depth > 3 && !improving);
+        reduction += 1000 * (depth > 3 && !improving);
 
         int reduced_depth = std::min(new_depth - reduction / 1024, ENGINE_MAX_DEPTH);
 
