d6dfdda59443a4fdd364dbc2e48fa634d63ac3ec
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-11-27 23:41:06 +0100

    update
    bench: 1013786

--- src/core.cpp
+++ src/core.cpp
@@ -803,7 +803,7 @@ int Engine::qsearch(int alpha, int beta, int depth, Stack* ss){
     }
 
     if (is_regular_eval(max_value) && is_regular_eval(beta) && max_value > beta)
-        max_value = (max_value + beta) / 2;
+        max_value = (2*max_value + beta) / 3;
 
     // assert(pos.isGameOver().second == GameResult::NONE);
     assert(is_valid(max_value));
