0ec45346b636f2c9b333c6cb4277dfb481c0a337
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-12-03 01:23:12 +0100

    update
    bench: 994852

--- src/core.cpp
+++ src/core.cpp
@@ -535,7 +535,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
         int reduction = 0;
     
         reduction -= red_1 * (gives_check && !root_node);
-        reduction += 800 * (!is_hit);
+        reduction += 600 * (!is_hit);
         reduction += red_2 * (move_gen.index() > 1 && !is_capture);
         reduction += red_3 * (tt_capture && !is_capture);
         reduction += red_4 * (move_gen.index() > lmr_1);
