f0bc3e1a03dd1312bbcade63e4f1ec7fd92cb16c
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-12-08 00:31:42 +0100

    update
    bench: 2877015

--- src/core.cpp
+++ src/core.cpp
@@ -811,7 +811,7 @@ int Engine::qsearch(int alpha, int beta, int depth, Stack* ss){
     if (pos.halfMoveClock() + depth + QSEARCH_MAX_DEPTH >= 100)
         return max_value;
 
-    if (depth == 0 || depth == -1)
+    if (depth == 0 || depth == -1 || depth == -2)
         transposition_table.store(zobrist_hash, to_tt(max_value, ply),
             stand_pat, DEPTH_QSEARCH, best_move,
             max_value >= beta ? TFlag::LOWER_BOUND : TFlag::UPPER_BOUND,
