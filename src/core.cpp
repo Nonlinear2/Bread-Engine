992b61bcac97f13e4874cd9df4af9770b4ae2331
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-12-07 19:26:59 +0100

    update
    bench: 3474166

--- src/core.cpp
+++ src/core.cpp
@@ -513,7 +513,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
                     return NO_VALUE;
     
                 if (value < singular_beta)
-                    extension = 1 + (value < singular_beta - 200);
+                    extension = 1 + (value < singular_beta - 300);
                 else if (value >= beta && !is_decisive(value))
                     return value;
             }
