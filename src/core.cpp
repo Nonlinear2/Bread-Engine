08082f886624bfdcbd560836d15abddb3caf9915
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-12-08 22:17:59 +0100

    update
    bench: 2988814

--- src/core.cpp
+++ src/core.cpp
@@ -433,7 +433,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
 
         // razoring
         if (eval + r_1*depth*depth + r_2 < alpha){
-            if (eval != transposition.value || transposition.flag != TFlag::EXACT)
+            if (eval != transposition.value || transposition.flag != TFlag::EXACT || transposition.depth == DEPTH_QSEARCH)
                 eval = qsearch<false>(alpha, beta, 0, ss + 1); // we update static eval to the better qsearch eval.
             if (eval <= alpha)
                 return eval;
