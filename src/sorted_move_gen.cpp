f5cfe3d98a6f696ec4230ffed25750fe99a6db0a
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-12-20 15:13:13 +0100

    update
    bench: 2605655

--- src/sorted_move_gen.cpp
+++ src/sorted_move_gen.cpp
@@ -72,12 +72,6 @@ void SortedMoveGen<movegen::MoveGenType::ALL>::set_score(Move& move){
     if (check_squares[static_cast<int>(piece.type())] & Bitboard::fromSquare(to))
         score += chk_1;
 
-    // captures should be searched early, so
-    // to_value = piece_value(to) - piece_value(from) doesn't seem to work.
-    // however, find a way to make these captures even better ?
-    if (to_piece != Piece::NONE)
-        score += cpt * piece_value[static_cast<int>(to_piece.type())] / 150;
-
     if (move.typeOf() == Move::PROMOTION)
         score += prm * piece_value[static_cast<int>(move.promotionType())] / 150;
 
@@ -85,8 +79,13 @@ void SortedMoveGen<movegen::MoveGenType::ALL>::set_score(Move& move){
     if (killer_moves.in_buffer(depth, move))
         score += kil;
 
-    // cant be less than worst move score
-    score += his * history.get(stm == Color::WHITE, from.index(), to.index()) / 10'000;
+    // captures should be searched early, so
+    // to_value = piece_value(to) - piece_value(from) doesn't seem to work.
+    if (to_piece != Piece::NONE)
+        score += cpt * piece_value[static_cast<int>(to_piece.type())] / 150;
+    else
+        score += his * history.get(stm == Color::WHITE, from.index(), to.index()) / 10'000;
+
 
     if (prev_piece != int(Piece::NONE) && prev_to != int(Square::underlying::NO_SQ))
         score += chis * cont_history.get(prev_piece, prev_to, piece, to.index()) / 10'000;
