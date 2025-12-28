e34df9f5f38fc3e5aa14987c821a2e9117db6883
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-12-28 18:59:30 +0100

    update
    bench: 2901254

--- src/core.cpp
+++ src/core.cpp
@@ -427,7 +427,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
     if (uncorrected_static_eval == NO_VALUE)
         uncorrected_static_eval = evaluate(pos);
 
-    static_eval = std::clamp(uncorrected_static_eval + pawn_corrhist.get(pos.sideToMove(), pos.get_pawn_key()) / 90,
+    static_eval = std::clamp(uncorrected_static_eval + pawn_corrhist.get(pos.sideToMove(), pos.get_pawn_key()) / 120,
         -BEST_VALUE, BEST_VALUE);
 
     ss->static_eval = static_eval;
@@ -740,7 +740,7 @@ int Engine::qsearch(int alpha, int beta, int depth, Stack* ss){
         if (!is_valid(static_eval))
             uncorrected_static_eval = evaluate(pos);
 
-        static_eval = std::clamp(uncorrected_static_eval + pawn_corrhist.get(pos.sideToMove(), pos.get_pawn_key()) / 90,
+        static_eval = std::clamp(uncorrected_static_eval + pawn_corrhist.get(pos.sideToMove(), pos.get_pawn_key()) / 120,
             -BEST_VALUE, BEST_VALUE);
 
         stand_pat = static_eval;
