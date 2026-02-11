fb92725cd03f874de0b00db400297196d99e65b7
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-02-11 12:04:39 +0100

    update
    bench: 2294699

--- src/core.cpp
+++ src/core.cpp
@@ -657,7 +657,8 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
     }
 
 
-    if (!in_check && !(best_move != Move::NO_MOVE && pos.isCapture(best_move))){
+    if (!in_check && !(best_move != Move::NO_MOVE && pos.isCapture(best_move))
+        && (max_value > ss->static_eval) == (best_move != Move::NO_MOVE)){
         int bonus = std::clamp((max_value - static_eval) * depth/7, -650, 650);
         pawn_corrhist.apply_bonus(pos.sideToMove(), pos.get_pawn_key(), bonus);
     }
