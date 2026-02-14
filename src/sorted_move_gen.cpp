e3bfa2fb461c90b38498ed384f8e9572464e230e
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-02-14 17:02:02 +0100

    update
    bench: 2847622

--- src/sorted_move_gen.cpp
+++ src/sorted_move_gen.cpp
@@ -69,47 +69,81 @@ void SortedMoveGen<GenType::QSEARCH>::prepare_pos_data(){
 // set move score to be sorted later
 template<>
 void SortedMoveGen<GenType::NORMAL>::set_score(Move& move){
-    
-    const Color stm = pos.sideToMove();
-    const Square from = move.from();
-    const Square to = move.to();
-    const Piece piece = pos.at(from);
-    const Piece to_piece = pos.at(to);
-    const int from_value = piece_value[piece.type()];
-    assert(piece.type() != PieceType::NONE);
+    if (stage == GENERATE_CAPTURES){
+        const Color stm = pos.sideToMove();
+        const Square from = move.from();
+        const Square to = move.to();
+        const Piece piece = pos.at(from);
+        const Piece to_piece = pos.at(to);
+        const int from_value = piece_value[piece.type()];
+        assert(piece.type() != PieceType::NONE);
 
-    int score = 0;
+        int score = 0;
 
-    if (piece.type() != PieceType::PAWN && piece.type() != PieceType::KING){
-        score += att_1 * bool(attacked_by_pawn & Bitboard::fromSquare(from)) * from_value / 150;
-        score -= att_2 * bool(attacked_by_pawn & Bitboard::fromSquare(to)) * from_value / 150;
+        if (piece.type() != PieceType::PAWN && piece.type() != PieceType::KING){
+            score += att_1 * bool(attacked_by_pawn & Bitboard::fromSquare(from)) * from_value / 150;
+            score -= att_2 * bool(attacked_by_pawn & Bitboard::fromSquare(to)) * from_value / 150;
+        }
+
+        if (check_squares[piece.type()] & Bitboard::fromSquare(to))
+            score += chk_1;
+
+        score += cpt * (piece_value[to_piece.type()] - from_value) / 150;
+
+        if (move.typeOf() == Move::PROMOTION)
+            score += prm * piece_value[move.promotionType()] / 150;
+
+        assert(depth != DEPTH_UNSEARCHED);
+        if (killer_moves.in_buffer(depth, move))
+            score += kil;
+
+        score += his * history.get(stm == Color::WHITE, from.index(), to.index()) / 10'000;
+
+        if (prev_piece != int(Piece::NONE) && prev_to != int(Square::underlying::NO_SQ))
+            score += chis * cont_history.get(prev_piece, prev_to, piece, to.index()) / 10'000;
+
+        score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
+
+        move.setScore(score);
+        return;
+
+    } else if (stage == GENERATE_QUIETS){
+
+        const Color stm = pos.sideToMove();
+        const Square from = move.from();
+        const Square to = move.to();
+        const Piece piece = pos.at(from);
+        const int from_value = piece_value[piece.type()];
+        assert(piece.type() != PieceType::NONE);
+
+        int score = 0;
+
+        if (piece.type() != PieceType::PAWN && piece.type() != PieceType::KING){
+            score += att_1 * bool(attacked_by_pawn & Bitboard::fromSquare(from)) * from_value / 150;
+            score -= att_2 * bool(attacked_by_pawn & Bitboard::fromSquare(to)) * from_value / 150;
+        }
+
+        if (check_squares[piece.type()] & Bitboard::fromSquare(to))
+            score += chk_1;
+
+        if (move.typeOf() == Move::PROMOTION)
+            score += prm * piece_value[move.promotionType()] / 150;
+
+        assert(depth != DEPTH_UNSEARCHED);
+        if (killer_moves.in_buffer(depth, move))
+            score += kil;
+
+        // cant be less than worst move score
+        score += his * history.get(stm == Color::WHITE, from.index(), to.index()) / 10'000;
+
+        if (prev_piece != int(Piece::NONE) && prev_to != int(Square::underlying::NO_SQ))
+            score += chis * cont_history.get(prev_piece, prev_to, piece, to.index()) / 10'000;
+
+        score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
+
+        move.setScore(score);
+        return;
     }
-
-    if (check_squares[piece.type()] & Bitboard::fromSquare(to))
-        score += chk_1;
-
-    // captures should be searched early, so
-    // to_value = piece_value(to) - piece_value(from) doesn't seem to work.
-    // however, find a way to make these captures even better ?
-    if (to_piece != Piece::NONE)
-        score += cpt * piece_value[to_piece.type()] / 150;
-
-    if (move.typeOf() == Move::PROMOTION)
-        score += prm * piece_value[move.promotionType()] / 150;
-
-    assert(depth != DEPTH_UNSEARCHED);
-    if (killer_moves.in_buffer(depth, move))
-        score += kil;
-
-    // cant be less than worst move score
-    score += his * history.get(stm == Color::WHITE, from.index(), to.index()) / 10'000;
-
-    if (prev_piece != int(Piece::NONE) && prev_to != int(Square::underlying::NO_SQ))
-        score += chis * cont_history.get(prev_piece, prev_to, piece, to.index()) / 10'000;
-
-    score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
-
-    move.setScore(score);
 }
 
 template<>
