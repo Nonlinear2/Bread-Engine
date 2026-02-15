61930fe4b6994d9334055c28497fa3bd13b353a7
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-02-15 01:18:00 +0100

    update
    bench: 2646286

--- src/sorted_move_gen.cpp
+++ src/sorted_move_gen.cpp
@@ -81,9 +81,9 @@ void SortedMoveGen<GenType::NORMAL>::set_score(Move& move){
         int score = 0;
 
         if (check_squares[piece.type()] & Bitboard::fromSquare(to))
-            score += 120;
+            score += chk_1;
 
-        score += cpt * (piece_value[to_piece.type()] - from_value) / 150;
+        score += 100 * (piece_value[to_piece.type()] - from_value) / 150;
 
         score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
 
