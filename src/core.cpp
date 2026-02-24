ea02e5c3981af9877389e03c222f15eec164f1fe
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-02-24 17:43:49 +0100

    prefetch tt before null move
    Yellow STC:
    Elo   | 0.73 +- 0.73 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | -3.01 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 344382 W: 98656 L: 97935 D: 147791
    Penta | [6932, 40824, 76126, 41209, 7100]
    https://nonlinear.eu.pythonanywhere.com/test/916/
    bench: 2659756

--- src/core.cpp
+++ src/core.cpp
@@ -484,9 +484,13 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
             ss->moved_piece = Piece::NONE;
             ss->current_move = Move::NULL_MOVE;
             ss->current_move_capture = false;
+
             pos.makeNullMove();
+            __builtin_prefetch(&transposition_table.entries[pos.hash() & (transposition_table.size - 1)]);
+
             int null_move_value = -negamax<false>(depth - R, -beta, -beta + 1, ss + 1, false);
             pos.unmakeNullMove();
+
             if (null_move_value >= beta && !is_win(null_move_value))
                 return null_move_value;
         }
