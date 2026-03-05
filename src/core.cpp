907fd52ad104bfc321d4fb4cb9561f51536fcd17
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-03-05 02:16:12 +0100

    update
    bench: 2514605

--- src/core.cpp
+++ src/core.cpp
@@ -590,7 +590,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
                 move_gen.update_cont_history(prev_piece, prev_to, ss->moved_piece, move.to(), -cont_2);
 
         } else if (!pv || move_gen.index() > 0){
-            value = -negamax<false>(new_depth, -alpha - 1, -alpha, ss + 1, !cutnode);
+            value = -negamax<false>(new_depth - (reduction > red_th_1), -alpha - 1, -alpha, ss + 1, !cutnode);
         }
 
         if (pv && (move_gen.index() == 0 || value > alpha)){
