755f7a8e637e82597766144dabc834fe41187fca
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-05-09 01:31:00 +0200

    update
    bench: 3833973

--- src/core.cpp
+++ src/core.cpp
@@ -584,7 +584,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
         new_depth -= depth > 5 && !is_hit; // IIR
         new_depth = std::min(new_depth, ENGINE_MAX_DEPTH);
 
-        int reduction = 100;
+        int reduction = 0;
 
         reduction -= red_1 * (gives_check && !root_node);
         reduction -= red_2 * (transposition.ttpv);
