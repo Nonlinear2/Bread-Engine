52d0535fc6df4e18646c749273eaa12fd505899b
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-05-12 02:10:04 +0200

    update
    bench: 3307827

--- src/core.cpp
+++ src/core.cpp
@@ -61,10 +61,6 @@ Engine::Engine(bool is_main_thread, TranspositionTable& tt, std::atomic<int64_t>
 
       
 int Engine::get_corrhist(chess::Color color){
-    uint16_t pk = pos.get_nonpawn_key(color);
-    pos.recompute_nonpawn_keys();
-    assert(pos.get_nonpawn_key(color) == pk);
-
     return (pawn_corrhist.get(color, pos.get_pawn_key()) 
            + nonpawn_corrhist[color].get(color, pos.get_nonpawn_key(color))
            + nonpawn_corrhist[color].get(~color, pos.get_nonpawn_key(~color))
