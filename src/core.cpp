67a729853b12c95a99ed55d2400588322fff7905
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-05-12 02:16:14 +0200

    update
    bench: 3004601

--- src/core.cpp
+++ src/core.cpp
@@ -62,8 +62,8 @@ Engine::Engine(bool is_main_thread, TranspositionTable& tt, std::atomic<int64_t>
       
 int Engine::get_corrhist(chess::Color color){
     return (corr_1 * pawn_corrhist.get(color, pos.get_pawn_key()) 
-           + 150 * nonpawn_corrhist[color].get(color, pos.get_nonpawn_key(color))
-           + 150 * nonpawn_corrhist[color].get(~color, pos.get_nonpawn_key(~color))
+           + 100 * nonpawn_corrhist[color].get(color, pos.get_nonpawn_key(color))
+           + 100 * nonpawn_corrhist[color].get(~color, pos.get_nonpawn_key(~color))
         ) / 32768;
 }
 
