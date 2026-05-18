4cda1e3434b8424a75dac888878f15db381dfb2f
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-05-18 12:01:08 +0200

    refactor histories
    passed non regression STC:
    Elo   | -0.54 +- 0.95 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.95 (-2.94, 2.94) [-3.50, 0.50]
    Games | N: 163302 W: 39594 L: 39846 D: 83862
    Penta | [1573, 19566, 39648, 19268, 1596]
    https://nonlinear.eu.pythonanywhere.com/test/1205/
    bench: 2905340

--- src/history.hpp
+++ src/history.hpp
@@ -8,75 +8,96 @@
 
 using namespace chess;
 
-class ContinuationHistory {
+UNACTIVE_TUNEABLE(CONTHIST_FILL_VALUE, int, -22, -5'000, 5'000, 50, 0.002);
+UNACTIVE_TUNEABLE(HIST_FILL_VALUE, int, 2, -5'000, 5'000, 50, 0.002);
+UNACTIVE_TUNEABLE(CAPTHIST_FILL_VALUE, int, -11, -5'000, 5'000, 50, 0.002);
+UNACTIVE_TUNEABLE(PAWN_CORRHIST_FILL_VALUE, int, 8, -5'000, 5'000, 50, 0.002);
+UNACTIVE_TUNEABLE(MINOR_CORRHIST_FILL_VALUE, int, 8, -5'000, 5'000, 50, 0.002);
+UNACTIVE_TUNEABLE(NONPAWN_CORRHIST_FILL_VALUE, int, 8, -5'000, 5'000, 50, 0.002);
+
+UNACTIVE_TUNEABLE(MAX_CONTHIST_BONUS, int, 10462, 0, 50'000, 2000, 0.002);
+UNACTIVE_TUNEABLE(MAX_HIST_BONUS, int, 9856, 0, 50'000, 2000, 0.002);
+UNACTIVE_TUNEABLE(MAX_CAPTHIST_BONUS, int, 9486, 0, 50'000, 2000, 0.002);
+UNACTIVE_TUNEABLE(MAX_PAWN_CORRHIST_BONUS, int, 7955, 0, 50'000, 2000, 0.002);
+UNACTIVE_TUNEABLE(MAX_MINOR_CORRHIST_BONUS, int, 7955, 0, 50'000, 2000, 0.002);
+UNACTIVE_TUNEABLE(MAX_NONPAWN_CORRHIST_BONUS, int, 7955, 0, 50'000, 2000, 0.002);
+
+template <std::size_t size, int fill_value, int max_bonus>
+class HistoryBase {
     public:
-    ContinuationHistory() { clear(); }
-    void clear();
+    HistoryBase() { clear(); }
+
+    void clear(){
+        std::fill(std::begin(history), std::end(history), fill_value);
+    }
+
+    void apply_bonus(int& value, int bonus){
+        value += bonus - value * std::abs(bonus) / max_bonus;
+    }
+
+    void save_to_stream(std::ofstream& ofs){
+        for (const auto& v : history)
+            ofs.write(reinterpret_cast<const char*>(&v), sizeof(int));
+    }
+
+    void load_from_stream(std::ifstream& ifs){
+        for (auto& v : history)
+            ifs.read(reinterpret_cast<char*>(&v), sizeof(int));
+    }
+
+    std::array<int, size> history = {};
+};
+
+class ContinuationHistory: public HistoryBase<
+        NUM_PIECES * NUM_SQUARES * NUM_PIECES * NUM_SQUARES,
+        CONTHIST_FILL_VALUE,
+        MAX_CONTHIST_BONUS
+    > {
+    public:
     int& get(Piece prev_piece, Square prev_to, Piece piece, Square to);
-    void apply_bonus(Piece prev_piece, Square prev_to, Piece piece, Square to, int bonus);
-    void save_to_stream(std::ofstream& ofs);
-    void load_from_stream(std::ifstream& ifs);
-
-    std::array<int, NUM_PIECES * NUM_SQUARES * NUM_PIECES * NUM_SQUARES> history = {};
 };
 
-class FromToHistory {
+class FromToHistory: public HistoryBase<
+        NUM_COLORS * NUM_SQUARES * NUM_SQUARES,
+        HIST_FILL_VALUE,
+        MAX_HIST_BONUS
+    > {
     public:
-    FromToHistory() { clear(); }
-    void clear();
     int& get(Color color, Square from, Square to);
-    void apply_bonus(Color color, Square from, Square to, int bonus);
-    void save_to_stream(std::ofstream& ofs);
-    void load_from_stream(std::ifstream& ifs);
-
-    std::array<int, NUM_COLORS * NUM_SQUARES * NUM_SQUARES> history = {};
 };
 
-class CaptureHistory {
+class CaptureHistory: public HistoryBase<
+        NUM_PIECES * NUM_SQUARES * NUM_PIECETYPES,
+        CAPTHIST_FILL_VALUE,
+        MAX_CAPTHIST_BONUS
+    > {
     public:
-    CaptureHistory() { clear(); }
-    void clear();
     int& get(Piece piece, Square to, Piece captured);
-    void apply_bonus(Piece piece, Square to, Piece captured, int bonus);
-    void save_to_stream(std::ofstream& ofs);
-    void load_from_stream(std::ifstream& ifs);
-
-    // [piece][to square][captured piece type]
-    std::array<int, 12*64*6> history = {};
 };
 
-class PawnCorrectionHistory {
+class PawnCorrectionHistory: public HistoryBase<
+        NUM_COLORS * PAWN_CORRHIST_SIZE,
+        PAWN_CORRHIST_FILL_VALUE,
+        MAX_PAWN_CORRHIST_BONUS
+    > {
     public:
-    PawnCorrectionHistory() { clear(); }
-    void clear();
     int& get(Color color, uint16_t pawn_key);
-    void apply_bonus(Color color, uint16_t pawn_key, int bonus);
-    void save_to_stream(std::ofstream& ofs);
-    void load_from_stream(std::ifstream& ifs);
-
-    std::array<int, NUM_COLORS * PAWN_CORRHIST_SIZE> history = {};
 };
 
-class MinorCorrectionHistory {
+class MinorCorrectionHistory: public HistoryBase<
+        NUM_COLORS * MINOR_CORRHIST_SIZE,
+        MINOR_CORRHIST_FILL_VALUE,
+        MAX_MINOR_CORRHIST_BONUS
+    > {
     public:
-    MinorCorrectionHistory() { clear(); }
-    void clear();
     int& get(Color color, uint16_t pawn_key);
-    void apply_bonus(Color color, uint16_t pawn_key, int bonus);
-    void save_to_stream(std::ofstream& ofs);
-    void load_from_stream(std::ifstream& ifs);
-
-    std::array<int, NUM_COLORS * MINOR_CORRHIST_SIZE> history = {};
 };
 
-class NonPawnCorrectionHistory {
+class NonPawnCorrectionHistory: public HistoryBase<
+        NUM_COLORS * NONPAWN_CORRHIST_SIZE,
+        NONPAWN_CORRHIST_FILL_VALUE,
+        MAX_NONPAWN_CORRHIST_BONUS
+    > {
     public:
-    NonPawnCorrectionHistory() { clear(); }
-    void clear();
     int& get(Color color, uint16_t pawn_key);
-    void apply_bonus(Color color, uint16_t pawn_key, int bonus);
-    void save_to_stream(std::ofstream& ofs);
-    void load_from_stream(std::ifstream& ifs);
-
-    std::array<int, NUM_COLORS * NONPAWN_CORRHIST_SIZE> history = {};
 };
