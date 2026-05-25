ea2d74c72dc8c039c24b843bebaf3ae770680a77
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-05-25 12:50:14 +0200

    spsa tune
    bench: 2864327

--- src/tune.hpp
+++ src/tune.hpp
@@ -5,7 +5,7 @@
 #include <set>
 #include <stdexcept>
 
-// #define IS_TUNE 1
+#define IS_TUNE 1
 
 namespace SPSA {
 
@@ -53,8 +53,8 @@ inline int get_value(const std::string& name){
 
 #ifdef IS_TUNE
     #define TUNEABLE(name, type, value, min, max, c, r) \
-        type name = value; \
-        static bool name##_init = []() { \
+        inline type name = value; \
+        inline bool name##_init = []() { \
             SPSA::register_tuneable(#name, &name, min, max); \
             std::cout << #name << ", " << #type << ", " << (float)(value) << ", " \
                       << (float)(min) << ", " << (float)(max) << ", " \
