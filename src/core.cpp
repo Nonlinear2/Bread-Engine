233facbedbc7f07e1d67fd783b016ac738f66b10
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-06-11 12:12:08 +0200

    simplify nmp reduction
    passed LTC non regression:
    Elo   | 7.14 +- 4.59 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.95 (-2.94, 2.94) [-3.50, 0.50]
    Games | N: 5746 W: 1349 L: 1231 D: 3166
    Penta | [16, 618, 1496, 718, 25]
    https://nonlinear.eu.pythonanywhere.com/test/1235/
    passed STC non regression:
    Elo   | 2.63 +- 3.29 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.98 (-2.94, 2.94) [-3.50, 0.50]
    Games | N: 15198 W: 3924 L: 3809 D: 7465
    Penta | [205, 1812, 3453, 1921, 208]
    https://nonlinear.eu.pythonanywhere.com/test/1233/
    bench: 2688139

--- src/core.cpp
+++ src/core.cpp
@@ -525,7 +525,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
         if (cutnode && (ss - 1)->curr_move != Move::NULL_MOVE && excluded_move == Move::NO_MOVE
             && eval > beta - depth*nmp_1 + nmp_2 && is_regular_eval(beta)){
 
-            int R = 3 + (eval >= beta) + depth / 4 + tt_capture;
+            int R = 4 + (eval >= beta) + depth / 4;
             ss->moved_piece = Piece::NONE;
             ss->curr_move = Move::NULL_MOVE;
             ss->curr_move_capture = false;
