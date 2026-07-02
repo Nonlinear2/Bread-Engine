469e4889492937b83acbee9b33cdc0450e86d89e
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-07-02 09:43:42 +0200

    add ft activation benchmark

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -37,6 +37,23 @@ int64_t sum(std::vector<int> values){
     return total;
 }
 
+std::vector<std::string> load_fens(const std::string& filename) {
+    std::vector<std::string> fens;
+    std::ifstream file(filename);
+    std::string line;
+    
+    if (!file.is_open()) {
+        std::cerr << "error, could not open file " << filename << std::endl;
+        return fens;
+    }
+    
+    while (std::getline(file, line))
+        fens.push_back(line);
+
+    file.close();
+    return fens;
+}
+
 void benchmark_nn(){
     std::cout << "============================== \n";
     std::cout << "evaluation function benchmark: \n";
@@ -83,6 +100,49 @@ void benchmark_engine(Engine& engine, int depth){
 
     engine.pos.setFen(constants::STARTPOS);
     engine.clear_state();
+}
+
+void benchmark_ft_activation(){
+    std::cout << "============================== \n";
+    std::cout << "accumulator activations benchmark: \n";
+
+    std::array<int, ACC_SIZE> zero_count = {0};
+
+    std::vector<std::string> fens = load_fens(bread_NNUE_FENS_PATH);
+
+    NnueBoard board = NnueBoard();
+    for (auto fen: fens){
+
+        board.setFen(fen);
+        board.synchronize();
+    
+        Color stm = board.sideToMove();
+        Accumulators& accumulators = board.accumulators_stack.top();
+        uint8_t ft_clamped_output[L1_INPUT_SIZE];
+    
+        NNUE_UTILS::pairwise_screlu16_to_8(
+            &accumulators[stm][0],
+            &accumulators[stm][ACC_SIZE / 2],
+            ft_clamped_output, ACC_SIZE / 2
+        );
+
+        NNUE_UTILS::pairwise_screlu16_to_8(
+            &accumulators[!stm][0],
+            &accumulators[!stm][ACC_SIZE / 2],
+            &ft_clamped_output[ACC_SIZE / 2], ACC_SIZE / 2
+        );
+
+        for (int j = 0; j < ACC_SIZE; j++){
+            zero_count[j] += (ft_clamped_output[j] == 0);
+        }
+    }
+
+    for (int i = 0; i < ACC_SIZE; i++){
+        std::cout << zero_count[i] << ", ";
+    }
+
+    std::cout << std::endl;
+    std::cout << "============================== \n";
 }
 
 } // namespace Benchmark
