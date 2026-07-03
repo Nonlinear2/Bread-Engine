0027228c2e080ff186c9f79a7dab6cd2a27cc94e
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-07-03 02:14:22 +0200

    update
    bench: 2045051

--- src/core.cpp
+++ src/core.cpp
@@ -676,8 +676,11 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
         if (value > max_value){
             assert(is_valid(value));
             max_value = value;
-            if (value > alpha)
+            if (value > alpha){
                 best_move = move;
+                if (!is_decisive(value) && depth >= 2 && depth < 8)
+                    depth--;
+            }
             if (root_node){
                 // ! This preserves the order of the array after the current move.
                 // ! Rotate invalidates root_moves[move_gen.index()].
