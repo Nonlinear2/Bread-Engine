a365361589e722f2f51c00ee3379d376aee864dd
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-07-24 13:43:52 +0200

    update lmp
    passed LTC:
    Elo   | 12.37 +- 4.75 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 3.00 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 5312 W: 1306 L: 1117 D: 2889
    Penta | [10, 549, 1356, 724, 17]
    https://nonlinear.eu.pythonanywhere.com/test/1391/
    passed STC:
    Elo   | 3.26 +- 2.28 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 3.02 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 28224 W: 7115 L: 6850 D: 14259
    Penta | [216, 3393, 6698, 3520, 285]
    https://nonlinear.eu.pythonanywhere.com/test/1390/
    bench: 2892313

--- src/core.cpp
+++ src/core.cpp
@@ -580,6 +580,9 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
                 && !is_hit && eval - lmp_1 * !improving < alpha)
                 continue;
 
+            if (!in_check && move_gen.index() > 3 && depth <= 8 && ss->static_eval + 200 + 85 * depth < alpha)
+                continue;
+
             // SEE pruning
             if (!in_check && move_gen.index() > 6 + depth / 2
                 && depth < 5 && !SEE::evaluate(pos, move, alpha - ss->static_eval - see_1 - see_2*depth))
