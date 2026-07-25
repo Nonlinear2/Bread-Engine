fcb51f0cd08e1a7445c00cbbf43ae38af016e300
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-07-26 01:00:13 +0200

    update
    bench: 2915371

--- src/core.cpp
+++ src/core.cpp
@@ -591,7 +591,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
 
                 // SEE pruning
                 if (move_gen.index() > 6 + depth / 2
-                    && depth < 5 && !SEE::evaluate(pos, move, alpha - ss->static_eval - see_1 - see_2*depth))
+                    && depth < 5 && !SEE::evaluate(pos, move, - see_1 - see_2*depth))
                     continue;
             } else {
                 if (move_gen.index() > 3 && depth <= 8 && ss->static_eval + lmp_2 + lmp_3 * depth < alpha)
@@ -610,7 +610,7 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
                 
                 // SEE pruning
                 if (move_gen.index() > 6 + depth / 2 &&
-                    depth < 5 && !SEE::evaluate(pos, move, -see_1 - see_2*depth))
+                    depth < 5 && !SEE::evaluate(pos, move, alpha - ss->static_eval - see_1 - see_2*depth))
                     continue;
             }
         }
