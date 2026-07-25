9061464a7a21969d503e6f75d988824675777d9f
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-07-26 00:58:38 +0200

    update
    bench: 2971898

--- src/core.cpp
+++ src/core.cpp
@@ -609,7 +609,8 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
                     continue;
                 
                 // SEE pruning
-                if (depth < 5 && !SEE::evaluate(pos, move, alpha - ss->static_eval - see_1 - see_2*depth))
+                if (move_gen.index() > 6 &&
+                    depth < 5 && !SEE::evaluate(pos, move, -100 - 50*depth))
                     continue;
             }
         }
