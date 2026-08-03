8674bf5e2879683b77d8d406545bb88bbf41c1a9
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-08-03 16:42:40 +0200

    update
    bench: 1960693

--- src/core.cpp
+++ src/core.cpp
@@ -595,16 +595,11 @@ int Engine::negamax(int depth, int alpha, int beta, Stack* ss, bool cutnode){
                 // no pruning
             } else if (is_capture){
 
-                if (move_gen.index() > 3
-                    && depth <= 8
-                    && ss->static_eval 
-                        + lmp_2
-                        + lmp_3 * depth
-                        + 50 * capt_history.get(from_piece, move.to(), to_piece) / 8192 < alpha)
+                if (move_gen.index() > 3 && depth <= 8 && ss->static_eval + lmp_2 + lmp_3 * depth)
                     continue;
 
                 // SEE pruning
-                if (move_gen.index() > 6 + depth / 2
+                if (move_gen.index() > 3 + depth / 2
                     && depth < 5 && !SEE::evaluate(pos, move, -see_1 - see_2*depth))
                     continue;
             } else {
