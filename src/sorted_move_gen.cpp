8736ec52a4f36dc6fcbfe2506adb96cafb326eb4
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-08-10 16:28:26 +0200

    use capture history for pruning margin
    passed LTC:
    Elo   | 1.52 +- 1.22 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 80432 W: 18402 L: 18051 D: 43979
    Penta | [221, 9337, 20754, 9678, 226]
    https://nonlinear.eu.pythonanywhere.com/test/1431/
    passed STC:
    Elo   | 2.76 +- 2.06 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.97 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 36248 W: 9398 L: 9110 D: 17740
    Penta | [391, 4278, 8523, 4516, 416]
    https://nonlinear.eu.pythonanywhere.com/test/1426/
    bench: 2184677

--- src/sorted_move_gen.cpp
+++ src/sorted_move_gen.cpp
@@ -115,7 +115,7 @@ void SortedMoveGen<GenType::NORMAL>::set_score(Move& move){
         if (move.typeOf() == Move::PROMOTION)
             score += c_prm * piece_value[move.promotionType()] / 256;
 
-        score += cphis * capt_history.get(piece, to.index(), to_piece) / 8192;
+        score += cphis * capt_history.get(piece, to, to_piece) / 8192;
 
         score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
 
@@ -149,13 +149,13 @@ void SortedMoveGen<GenType::NORMAL>::set_score(Move& move){
             score += q_kil;
 
         // cant be less than worst move score
-        score += q_his * history.get(stm, from.index(), to.index()) / 8192;
+        score += q_his * history.get(stm, from.index(), to) / 8192;
 
         if (prev_piece != int(Piece::NONE) && prev_to != int(Square::underlying::NO_SQ))
-            score += q_cthis * cont_history.get(prev_piece, prev_to, piece, to.index()) / 8192;
+            score += q_cthis * cont_history.get(prev_piece, prev_to, piece, to) / 8192;
 
         if (prev_piece2 != int(Piece::NONE) && prev_to2 != int(Square::underlying::NO_SQ))
-            score += q_cthis2 * cont_history2.get(prev_piece2, prev_to2, piece, to.index()) / 8192;
+            score += q_cthis2 * cont_history2.get(prev_piece2, prev_to2, piece, to) / 8192;
 
         score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
 
