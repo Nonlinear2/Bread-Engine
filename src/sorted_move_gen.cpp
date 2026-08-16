a0cc9be2b402387e2620f59f56b1e613a20f8f4c
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-08-16 15:14:44 +0200

    update
    bench: 2118384

--- src/sorted_move_gen.cpp
+++ src/sorted_move_gen.cpp
@@ -164,10 +164,8 @@ void SortedMoveGen<GenType::QSEARCH>::set_score(Move& move){
     const Piece to_piece = pos.at(move.to());
 
     int score = 0;
-    if (to_piece != Piece::NONE || move.typeOf() == Move::ENPASSANT){
-        score += c_cpt * piece_value[to_piece.type()] / 256;
-        score += 100 * capt_history.get(piece, move.to(), to_piece) / 8192;
-    }
+    score += piece_value[to_piece.type()];
+    score += 100 * capt_history.get(piece, move.to(), to_piece) / 8192;
 
     score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
 
