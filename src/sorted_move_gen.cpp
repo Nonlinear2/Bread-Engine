e4ef9d92f956b36d0af66cf2884f6b3428a75202
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2026-08-24 14:19:32 +0200

    use capthist in qs move ordering
    passed LTC:
    Elo   | 1.01 +- 0.70 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 249142 W: 57271 L: 56546 D: 135325
    Penta | [773, 29088, 64108, 29845, 757]
    https://nonlinear.eu.pythonanywhere.com/test/1450/
    passed STC:
    Elo   | 2.73 +- 2.05 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 36722 W: 9447 L: 9158 D: 18117
    Penta | [364, 4434, 8552, 4571, 440]
    https://nonlinear.eu.pythonanywhere.com/test/1446/
    bench: 2396357

--- src/sorted_move_gen.cpp
+++ src/sorted_move_gen.cpp
@@ -160,8 +160,14 @@ void SortedMoveGen<GenType::NORMAL>::set_score(Move& move){
 
 template<>
 void SortedMoveGen<GenType::QSEARCH>::set_score(Move& move){
+    const Piece piece = pos.at(move.from());
+    const Piece to_piece = pos.at(move.to());
 
-    int score = piece_value[pos.at(move.to()).type()];
+    int score = 0;
+    if (to_piece != Piece::NONE || move.typeOf() == Move::ENPASSANT){
+        score += c_cpt * piece_value[to_piece.type()] / 256;
+        score += 100 * capt_history.get(piece, move.to(), to_piece) / 8192;
+    }
 
     score = std::clamp(score, WORST_MOVE_SCORE + 1, BEST_MOVE_SCORE - 1);
 
